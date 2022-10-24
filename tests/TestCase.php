3556286295d72828156bb7e11267df9764e3e48a
Bastien Philippe <bastien.philippe@soyhuce.fr>
2022-10-24 08:03:01 +0200

    Run tests without deprecation handling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,19 @@
 
 namespace Test;
 
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDeprecationHandling;
 use Soyhuce\Docker\ServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    use InteractsWithDeprecationHandling;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutDeprecationHandling();
+    }
+
     protected function getPackageProviders($app)
     {
         return [
